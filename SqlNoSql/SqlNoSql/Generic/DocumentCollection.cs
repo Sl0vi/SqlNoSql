960f5a2119cc22b2364882ff0a86197dd601d3b8
Bernhard Johannessen <bernhard@voytsje.com>
2014-04-11 19:40:33 +0100

    Added interfaces and class layouts

---
+++ SqlNoSql/SqlNoSql/Generic/DocumentCollection.cs
@@ -0,0 +1,89 @@
+﻿// The MIT License (MIT)
+//
+// Copyright (c) 2014 Bernhard Johannessen
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+namespace SqlNoSql.Generic
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DocumentStore<T> : IDocumentCollection<T>
+    {
+        public T this[Guid key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public T Find(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(Func<T, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public KeyValuePair<Guid, T> FindWithKey(Func<T, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> Filter(Func<T, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<KeyValuePair<Guid, T>> FilterWithKeys(Func<T, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddOrUpdate(Guid key, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SqlNoSql/SqlNoSql/Generic/IDocumentCollection.cs
@@ -0,0 +1,43 @@
+﻿// The MIT License (MIT)
+//
+// Copyright (c) 2014 Bernhard Johannessen
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace SqlNoSql.Generic
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A collection of BSON or JSON documents that can be deserialized to the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of objects contained in the collection</typeparam>
+    public interface IDocumentCollection<T> : IEnumerable<T>
+    {
+        T this[Guid key] { get; set; }
+        T Find(Guid key);
+        T Find(Func<T, bool> filter);
+        KeyValuePair<Guid, T> FindWithKey(Func<T, bool> filter);
+        ICollection<T> Filter(Func<T, bool> filter);
+        ICollection<KeyValuePair<Guid, T>> FilterWithKeys(Func<T, bool> filter);
+        void AddOrUpdate(Guid key, T item);
+        void Remove(Guid key);
+    }
+}
